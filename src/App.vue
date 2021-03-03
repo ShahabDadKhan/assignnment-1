c969b3b0da9994aac5fbba94e6710b2d163a3ecc
Shahab Dad khan <shahabkhan681@gmail.com>
2021-03-03 16:07:45 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <active-user :username="user.name" :userage="user.age"></active-user>
+    <user-data @set-data="setUserData"></user-data>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Shahab Dad Khan",
+        age: 13,
+      },
+    };
+  },
+
+  methods: {
+    setUserData(name, age) {
+      this.user = {
+        name: name,
+        age: age,
+      };
+    },
+  },
+};
+</script>
---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <h2>
+      {{ username }}
+    </h2>
+    <h3>{{ userage }} years</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ["username", "userage"],
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+}
+</style>
